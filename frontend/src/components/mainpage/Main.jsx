5a6ed2f173cfa8a0160f8f1fb325958e9c7a1ef5
HRZXD <chayakorn251@gmail.com>
2024-07-20 14:48:51 +0700

    UPDATE IG AND GITHUB LINK

--- frontend/src/components/mainpage/Main.jsx
+++ frontend/src/components/mainpage/Main.jsx
@@ -127,13 +127,13 @@ function Main() {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] w-72 p-4 shadow"
             >
               <li>
-                <a className="justify-between text-base">
+                <a className="justify-between text-base" href="https://www.instagram.com/_hr1pct/" target="_blank">
                   Instagram
                   <span className="badge">New</span>
                 </a>
               </li>
               <li>
-                <a className="text-base">Github</a>
+                <a className="text-base" href="https://github.com/HRZXD" target="_blank">Github</a>
               </li>
             </ul>
           </div>
@@ -210,7 +210,7 @@ function Main() {
           <nav>
             <div className="text-base font-bold mb-2">My Socials</div>
             <div className="grid grid-flow-col gap-4">
-              <a className="mr-9 text-neutral-800 dark:text-neutral-200 btn btn-square">
+              <a className="mr-9 text-neutral-800 dark:text-neutral-200 btn btn-square" href="https://www.instagram.com/_hr1pct/" target="_blank">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-10 w-10"
@@ -220,7 +220,7 @@ function Main() {
                   <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
                 </svg>
               </a>
-              <a className="text-neutral-800 dark:text-neutral-200 btn btn-circle">
+              <a className="text-neutral-800 dark:text-neutral-200 btn btn-circle" href="https://github.com/HRZXD" target="_blank">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-10 w-10"
