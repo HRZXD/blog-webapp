df4ae039c7993b264dbe3cc97bf04a814e9212e4
HRZXD <chayakorn251@gmail.com>
2024-07-20 19:28:01 +0700

    UPDATE NODE JS

--- backend/index.js
+++ backend/index.js
@@ -5,94 +5,93 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const fs = require('fs');
 const app = express();
-app.use(express.json())
+require('dotenv').config();
+
+app.use(express.json());
 app.use(cors());
 
-const Admin = require('./model/adminModel')
-const Blog = require('./model/blogModels')
+const Admin = require('./model/adminModel');
+const Blog = require('./model/blogModels');
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
+
 mongoose.set('strictQuery', false);
 
-mongoose.connect('mongodb+srv://adminhrz:PhtfuMrarlLHv4Qf@cluster0.ewbiuim.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' ,{
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
-}).then(()=>{
-    console.log("mongodb connected");
-}).catch(() => {
-    console.error;
-})
-app.get("/", (req, res) => {
-  res.send("Default Route Works!");
-})
-app.post('/admin' , async (req, res) => {
-    const {username, password} = req.body;
-    try{
-        await Admin.findOne({username: username, password: password})
-        .then(admin => {
-            if (admin){
-                if(admin.password === password){
-                    res.send("success")
-                }else{
-                    res.status(500).send("Error occurred while inserting booking.");
-                }
-            }
-            console.log(admin);
-        })
-    }catch(err){
-        console.error(err);
+}).then(() => {
+  console.log("MongoDB connected");
+  app.get("/", (req, res) => {
+    res.send("Default Route Works!");
+  });
+}).catch((err) => {
+  console.error("Error connecting to MongoDB:", err.message);
+});
+
+app.post('/admin', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const admin = await Admin.findOne({ username: username, password: password });
+    if (admin) {
+      if (admin.password === password) {
+        res.send("success");
+      } else {
         res.status(500).send("Error occurred while inserting booking.");
+      }
     }
-})
+    console.log(admin);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error occurred while inserting booking.");
+  }
+});
 
 app.post('/blogs', upload.single('file'), async (req, res) => {
-    const newBlog = new Blog({
-      topic: req.body.topic,
-      story: req.body.story,
-      date: req.body.date,
-      detail: req.body.detail,
-      img: req.file ? {
-        data: req.file.buffer,
-        contentType: req.file.mimetype
-      } : null
-    });
-    try {
-        await newBlog.save();
-        res.status(200).json({ message: 'Blog post created successfully' });
-      } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  const newBlog = new Blog({
+    topic: req.body.topic,
+    story: req.body.story,
+    date: req.body.date,
+    detail: req.body.detail,
+    img: req.file ? {
+      data: req.file.buffer,
+      contentType: req.file.mimetype
+    } : null
+  });
+  try {
+    await newBlog.save();
+    res.status(200).json({ message: 'Blog post created successfully' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.get('/blogs', async (req, res) => {
-    try {
-      const blogs = await Blog.find({});
-      res.status(200).json(blogs);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
+  try {
+    const blogs = await Blog.find({});
+    res.status(200).json(blogs);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
-  
-  // Route to fetch image by ID
+
+// Route to fetch image by ID
 app.get('/image/:id', async (req, res) => {
-    try {
-      const blog = await Blog.findById(req.params.id);
-      if (!blog || !blog.img) {
-        res.status(404).json({ error: 'Image not found' });
-      } else {
-        res.contentType(blog.img.contentType);
-        res.send(blog.img.data);
-      }
-    } catch (err) {
-      res.status(500).json({ error: err.message });
+  try {
+    const blog = await Blog.findById(req.params.id);
+    if (!blog || !blog.img) {
+      res.status(404).json({ error: 'Image not found' });
+    } else {
+      res.contentType(blog.img.contentType);
+      res.send(blog.img.data);
     }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-app.listen(4000, () => {
-    console.log("Server listening on 4000");
-})
-
-
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on ${process.env.PORT}`);
+});
 
